7efc5cec84c54441115855c30caccf37e29a5860
Jose Maria Farfan <magoalakai@gmail.com>
2021-03-30 22:10:15 +0200

    CRUD completo

--- resources/views/auth/password.blade.php
+++ resources/views/auth/password.blade.php
@@ -7,14 +7,14 @@
 <form method="POST" class="row col-8 mx-auto">
     @csrf
     <div class="mb-3 col-8 mx-auto">
-        <label class="form-label">Password</label>
+        <label class="form-label">New password</label>
         <input type="password" class="form-control @error('password') is-invalid @enderror" id="password" name="password">
         @error('password')
             <div class="alert alert-danger">{{ $message }}</div>
         @enderror
     </div>
     <div class="mb-3 col-8 mx-auto">
-        <label class="form-label">New Password</label>
+        <label class="form-label">Confirm new password</label>
         <input type="password" class="form-control @error('newpassword') is-invalid @enderror" id="newpassword" name="newpassword">
         @error('newpassword')
             <div class="alert alert-danger">{{ $message }}</div>
